b48db0019043c0422bba1762b4a900947a394fd2
Eric Lee <ayrocklee@gmail.com>
2024-05-03 13:49:22 -0700

    add root suspense

--- src/app/(site)/layout.js
+++ src/app/(site)/layout.js
@@ -1,8 +1,15 @@
+import React from "react";
 import { UseFathom } from "@/lib/use-fathom";
 
 export default function SiteLayout({ children }) {
   return (
-    <>
+    <React.Suspense
+      fallback={
+        <main className="flex flex-1 items-center justify-center">
+          <span>Loading...</span>
+        </main>
+      }
+    >
       <UseFathom />
       <header className="h-12">
         <nav className="h-full flex gap-4 items-center mx-4">
@@ -11,6 +18,6 @@ export default function SiteLayout({ children }) {
         </nav>
       </header>
       {children}
-    </>
+    </React.Suspense>
   );
 }
